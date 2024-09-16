a82ad96ac1c99827dc185f910b207d434498be58
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-09-16 09:17:36 +0300

    Update oop_pamati.py

--- oop_pamati.py
+++ oop_pamati.py
@@ -6,7 +6,14 @@ class person:
         print(self.name,self.age)
     def labdien(self):
         print (f"Labdien, {self.name}!")
+
+
 p=person()
-p.set("Peter", 20)
+p.set("Lina", 19)
 p.output()
 p.labdien()
+d=person()
+d.set("Alesja", 18)
+d.output()
+
+
