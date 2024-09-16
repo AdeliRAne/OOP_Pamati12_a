66e05434d7da5512a5f5d7c1ef5d830d4227daf2
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-09-16 09:17:55 +0300

    Update oop_pamati.py

--- oop_pamati.py
+++ oop_pamati.py
@@ -7,11 +7,19 @@ class person:
     def labdien(self):
         print (f"Labdien, {self.name}!")
 
+p=person()
+sk=int(input("Ievadiet cilvēku skaits: "))
+for i in range(sk):
+    name=input("Ievadiet vārdu: ")
+    age=input("Ievadiet vecumu: ")
+    person.set(p,name,age)
+    person.output(p)
 
 p=person()
 p.set("Lina", 19)
 p.output()
 p.labdien()
+
 d=person()
 d.set("Alesja", 18)
 d.output()
