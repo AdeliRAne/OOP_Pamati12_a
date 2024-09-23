78bd1b9488eac3a5ae8a24230466497f6d03e5c6
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-09-23 10:22:12 +0300

    Update t_uzdevums1.py

--- t_uzdevums1.py
+++ t_uzdevums1.py
@@ -6,5 +6,26 @@ class BankAccount:
     def get_balance(self):
         return self.__balance
     
-account = BankAccount(1000)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f'{amount} Eur noguldīti kontā.')
+        else:
+            print('Noguldīšanas summa jābūt pozitīvai.')
+
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            print('Nepietiekami līdzekļi izņemšana.')
+        elif amount <=0:
+            print('Izņemošanas summa jābūt pozitīvasi.')
+        else:
+            self.__balance -= amount
+            print(f'{amount} Eur izņemti no konta.')
+    
+account = BankAccount(100)
 print('Pašreizējais atlikums:', account.get_balance())
+account.deposit(50)
+print('Pašreizējais atlikums pēc noguldījuma:', account.get_balance())
+account.withdraw(200)
+account.withdraw(30)
+print('Pašreizējais atlikums pēc izņemšanas: ', account.get_balance())
