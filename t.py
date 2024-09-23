45d74a30ae9e307f46e0658501b256f4c3a34286
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-09-23 09:07:02 +0300

    Create t.py

---
+++ t.py
@@ -0,0 +1,23 @@
+class Animal:
+    legs = 4
+    tail = 1
+
+    def voice(self):
+        print('Kaut-kāda skaņa')
+
+class Dog(Animal):
+    def voice(self):
+        print('Woof')
+        
+class Cat(Animal):
+    def voice(self):
+        print('Meow')
+
+anim=Animal()
+anim.voice()
+dog1, dog2 = Dog(),Dog()
+cat1, cat2 = Cat(), Cat()
+
+farm_band=[cat1, cat2, dog1, dog2]
+for i in farm_band:
+    i.voice()
