33c7a7cb4a1187ef50baa9c58d9d6140a21c2a44
AdeliRAne <125028905+AdeliRAne@users.noreply.github.com>
2024-10-01 10:17:26 +0300

    Исправления ошибок
    новый класс и т.д

--- deja_pulcina_PD.py
+++ deja_pulcina_PD.py
@@ -1,24 +1,24 @@
 import itertools
 from datetime import time
-class Klients:
-    Klienta_vaards = " "
-    Klienta_uzvaards = " "
-    Klienta_vecums = " "
-    Klienta_PK = " "
-    Klienta_adrese = " "
-    id_iter = itertools.count()
 
-    def init(self, vaards, uzvaards, vecums, personalais_kods, adrese ):
+class Klients:
+    id_iter = itertools.count()
+    
+    def __init__(self, vaards, uzvaards, vecums, personalais_kods, adrese, cena_par_men, dienas, nodarbiiba_laiks):
         self.Klienta_id = next(self.id_iter)
         self.Klienta_vaards = vaards
         self.Klienta_uzvaards = uzvaards
         self.Klienta_vecums = vecums
         self.Klienta_PK = personalais_kods
         self.Klienta_adrese = adrese
+        self.cena_par_men = cena_par_men
+        self.dienas = dienas
+        self.nodarbiiba_laiks = nodarbiiba_laiks
+
 
     def Klienta_info(self):
-        return[
-            self.Klienta_vaards, self.Klienta_uzvaards,self.Klienta_vecums, self.Klienta_PK, self.Klienta_adrese
+        return [
+            self.Klienta_vaards, self.Klienta_uzvaards, self.Klienta_vecums, self.Klienta_PK, self.Klienta_adrese
         ]
  
     def Klienta_info_print(self):
@@ -26,20 +26,34 @@ class Klients:
         print("Bērna uzvārds: " + str(self.Klienta_uzvaards))
         print("Bērna vecums: " + str(self.Klienta_vecums))
         print("Bērna personalais kods: " + str(self.Klienta_PK))
-        print("Bērna adrese: " + str(self.Klienta_adrese))
+        print("Adrese: " + str(self.Klienta_adrese))
+        print("-" * 100)
 
-class Vecaaks(Klients):
-    def init(self, vaards, uzvaards, vecums, personalais_kods, adrese, tel_number ):
-        self.Klienta_id = next(self.id_iter)
-        self.Klienta_vaards = vaards
-        self.Klienta_uzvaards = uzvaards
-        self.Klienta_vecums = vecums
-        self.Klienta_PK = personalais_kods
-        self.Klienta_adrese = adrese
+    def nodarbiiba_laiks_info(self, cena, nedelas_dienas, laiks):
+        print(f"Cena par mēnesi: {self.cena_par_men} EUR")
+        print("Dienas, kad notiek nodarbības: " + ", ".join(self.dienas))
+        print(f"Nodarbību laiks: {self.nodarbiiba_laiks.strftime('%H:%M')}")
+        print("-" * 100)
+
+        
+class Vecaaks:
+    
+    def __init__(self, vaards, uzvaards, tel_number):
+        self.Vecaaka_vaards = vaards
+        self.Vecaaka_uzvaards = uzvaards
         self.tel_number = tel_number
+
+    def Vecaaks_info_print(self):
+        print("Vecāka vārds: " + str(self.Vecaaka_vaards))
+        print("Vecāka uzvārds: " + str(self.Vecaaka_uzvaards))
+        print("Vecāka telefona numurs: " + str(self.tel_number))
+        print("-" * 100)
  
 
-k1 = Klients("Alina", "ikova", "3", 120621-25415, "Rezeke, Pils iela 1"  )
-print(k1.Klienta_id)
-k1.Klienta_info()
-k1.Klienta_info_print()  
+k1 = Klients("Alīna", "Suškova", "3", "120621-25415", "Rēzekne, Pils iela 1", 25, ["Pirmdiena", "Trešdiena", "Piektdiena"], time(17, 30))
+print(f"Klienta ID: {k1.Klienta_id}")
+k1.Klienta_info_print()
+k1.nodarbiiba_laiks_info(25, ["Pirmdiena", "Trešdiena", "Piektdiena"], time(17, 30))
+
+v1 = Vecaaks("Artemijs", "Suškovs", "+371 23345678")
+v1.Vecaaks_info_print()
