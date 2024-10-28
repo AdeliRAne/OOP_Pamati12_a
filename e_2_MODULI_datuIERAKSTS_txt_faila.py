e458888aaab88fdd2ab1cd523bb31654a6671a2e
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-10-28 08:38:30 +0200

    s
    s

--- e_2_MODULI_datuIERAKSTS_txt_faila.py
+++ e_2_MODULI_datuIERAKSTS_txt_faila.py
@@ -1,3 +1,4 @@
+"""
 with open("data.txt", "r", encoding="utf-8") as file:
     saturs = file.read()
 print(saturs)
@@ -8,3 +9,24 @@ with open("data.txt", "r", encoding="utf-8") as file:
 print(rindas) 
 for rinda in rindas:
     print(rinda.strip()) #Izvada katru rindiņu, noņemot liekās atstarpes
+
+with open("data.txt", "r", encoding="utf-8") as file:
+    for rinda in file:
+        print(rinda.strip())
+#-----------------------------------------------------------------------------------
+#UZD - Cik vārdus teikuma
+with open("teksts.txt", "r", encoding="utf-8") as file:
+    saturs = file.read()
+vaardi = saturs.split()
+un_vaardi = len(set(vaardi))
+print(vaardi)
+print(len(vaardi))
+print("Unikālo vārdu skaits: ", un_vaardi)"""
+#-----------------------------------------------------------------------------------
+#UZD - Rindu numerācija un izvade
+#enumerate()
+with open("data.txt", "r", encoding="utf-8") as file:
+    rindas = file.readlines()
+for index, rinda in enumerate(rindas, start=1):
+    if rinda.strip():
+        print(f"{index}: {rinda.strip()}")
