bf8d9b9e5ad009651b7073486ee6a44756c43d83
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-11-04 09:02:38 +0200

    a
    a

---
+++ ORGANISATION/organisations.py
@@ -0,0 +1,9 @@
+organizations=[]
+organizations_a={
+    'name':'Tesla Motors',
+    'adress':'Tesla ave 1, USA',
+    'id':'8646678'
+}
+
+organizations.append(organizations_a)
+print(organizations)
