524aec6b1f77e4699a1b710d2998b15dd1a7bf22
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-11-04 10:39:27 +0200

    a
    a

--- ORGANISATION/organisations.py
+++ ORGANISATION/organisations.py
@@ -2,8 +2,43 @@ organizations=[]
 organizations_a={
     'name':'Tesla Motors',
     'adress':'Tesla ave 1, USA',
-    'id':'8646678'
+    'id':'8646678',
+    'contacts':[
+        {
+            'name':'Elon',
+            'position':'CEO',
+            'id':1
+        },
+        {
+            'name':'Vlas',
+            'position':'CTO',
+            'id':2        
+        }
+    ]
 }
-
+organizations_b={
+    'name':'Eesti posti',
+    'adress':'Super street 2, Tartu',
+    'id':'1295843',
+    'contacts':[
+        {
+            'name':'Anna',
+            'position':'CEO',
+            'id':3
+        },
+        {
+            'name':'Alina',
+            'position':'CTO',
+            'id':4        
+        }
+    ]
+}
 organizations.append(organizations_a)
+organizations.append(organizations_b)
 print(organizations)
+
+for organization in organizations:
+    print('---ORGANIZATION---')
+    print(f"{organization['name']} ({organization['id']})")
+    print(f"Adrese:{organization['adress']}")
+    print(f"Kontaktu skaits: {len(organization['contacts'])}")
