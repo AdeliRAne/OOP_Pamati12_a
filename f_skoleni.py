1142868cc321b13dab1166f88d17713383604e66
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-11-04 08:03:04 +0200

    a
    a

--- f_skoleni.py
+++ f_skoleni.py
@@ -4,6 +4,6 @@ with open ('f_skoleni.json', 'r', encoding="utf-8") as file:
     print(skoleni)
 
 print("Cilvēks, kuram vērtējums ir vairāk par 4 un mazāk par 7: ")
-for vairak in skoleni:
-    if vairak["vertejums"] > 4 and vairak["vertejums"] < 7:
-        print(vairak["vards"])
+for v in skoleni:
+    if v["vertejums"] > 4 and v["vertejums"] < 7:
+        print(v["vards"])
