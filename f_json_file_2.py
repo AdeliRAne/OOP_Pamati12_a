2f1a18c5ab459e47e30eff481e7f02446fc9b554
shaqqwe <119863126+shaqqwe@users.noreply.github.com>
2024-11-04 08:43:20 +0200

    d
    d

---
+++ f_json_file_2.py
@@ -0,0 +1,28 @@
+import json
+""""""
+#Ieraksta tikai vienu cilvēku
+try:
+    with open ('user_data.json', 'r', encoding="utf-8") as file:
+        data = json.load(file) 
+except FileNotFoundError:
+    data = []
+
+while True:
+    name = input("Ievadi vārdu: ")
+    age = input("Ievadi vecums: ")
+    city = input("Ievadi pilsētu: ") 
+    
+    data.append({
+    "name": name,
+    "age" : age,
+    "city" : city
+    })
+    another = input("VAi vēlies pievienot vēl vienu cilvēku? (jā/nē)").strip().lower()
+    if another !="jā":
+        break
+
+with open ('data.json', 'w', encoding="utf-8") as file:
+    json.dump(data, file, indent=4) 
+print("Dati ir veiksmīgi saglabāti JSON failā!")
+
+ 
